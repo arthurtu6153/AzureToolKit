a7c51a1f2d6c0f8e181199021372ab7fc59cfbec
arthurtu6153 <42453609+arthurtu6153@users.noreply.github.com>
2019-03-19 11:36:20 -0700

    service

--- AzureToolKit/ClientApp/src/app/app.server.module.ts
+++ AzureToolKit/ClientApp/src/app/app.server.module.ts
@@ -3,6 +3,7 @@ import { ServerModule } from '@angular/platform-server';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 import { AppComponent } from './app.component';
 import { AppModule } from './app.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [AppModule, ServerModule, ModuleMapLoaderModule],
--- AzureToolKit/ClientApp/src/app/home/home.component.ts
+++ AzureToolKit/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DateService } from '../common/services/date.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  public currentDate: Date
+  constructor(private dateService: DateService){
+    this.currentDate = dateService.getCurrentDate();
+  }
+
+  ngOnInit(): void{}
 }
